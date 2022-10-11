a489f2a4d824dea7896d77a71007dfe61e976c96
Jordan Ibarra Ashurst <bxordan@gmail.com>
2022-10-10 22:42:12 -0700

    initializing answer sheet and set data method

--- opengrader.py
+++ opengrader.py
@@ -1,12 +1,21 @@
+from concurrent.futures import process
 import cv2 as cv
 import numpy as np
+from answersheet import AnswerSheet
+# from answersheet import AnswerSheet
 from document import DocumentProcessor
+
 def main():
     img = cv.imread('sheet/answered.jpg')
     processor = DocumentProcessor(img)
     processor.process()
-    processor._write_steps()
-
+    # processor._write_steps()
+    
+    points = processor.get_choice_points()
+    intensities = processor.get_intensities()
+    answer_sheet = AnswerSheet(points, intensities)
+    answer_sheet.set_data()
+    # AnswerSheet()
 
 if __name__ == '__main__':
     main()
