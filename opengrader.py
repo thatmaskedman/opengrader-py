513d2553b504da642a268599e868d1e259902931
Jordan Ibarra Ashurst <bxordan@gmail.com>
2022-12-01 16:57:31 -0700

    tunning entry point

--- opengrader.py
+++ opengrader.py
@@ -6,18 +6,20 @@ from answersheet import AnswerSheet
 from document import DocumentProcessor
 
 def main():
-    img = cv.imread('sheet/answered.jpg')
+    question_count = 50
+
+    img = cv.imread('sheet/foobaz.png')
     processor = DocumentProcessor(img)
     processor.process()
-    # processor._write_steps()
+    processor._write_steps()
     
     points = processor.get_choice_points()
     intensities = processor.get_intensities()
     
-    answer_sheet = AnswerSheet(processor.img_scaled, points, intensities)
+    answer_sheet = AnswerSheet(processor.img_scaled, points, intensities, question_count)
     answer_sheet.set_data()
-    answer_sheet.grade()
-    answer_sheet.mark_grade()
+    # answer_sheet.grade()
+    # answer_sheet.mark_grade()
     cv.imwrite('graded.jpg', answer_sheet.img)
     # AnswerSheet()
 
