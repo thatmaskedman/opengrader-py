e5abfaaf1af2febf024480f61d9c5421ef56bea4
Jordan Ibarra Ashurst <bxordan@gmail.com>
2022-12-03 03:06:34 -0700

    Rewriting argparse.

--- opengrader.py
+++ opengrader.py
@@ -1,17 +1,23 @@
 import argparse
-
 import cv2 as cv
 import numpy as np
 from answersheet import AnswerSheet
-# from answersheet import AnswerSheet
 from document import DocumentProcessor
+from api_client import APIClient
+import serializers
+import json
 
 def main():
     parser = argparse.ArgumentParser(
         description='Opengrader CLI Tool'
     )
 
-    parser.add_argument('--exam', help='Path to the exam answer sheet.')
+    parser.add_argument('-e', '--exam', help='Path to the exam answer sheet.', required=True)
+    parser.add_argument('-k', '--keysheet', help='Path to the Key Sheet JSON answer file.')
+    
+    parser.add_argument('--grade', help='Attempt to grade.')
+    parser.add_argument('--out', help='Specify output directory.')
+    parser.add_argument('--url', help='Base URL')
     args = parser.parse_args()
 
     if args.exam:
@@ -25,9 +31,29 @@ def main():
     
         answer_sheet = AnswerSheet(processor.img_scaled, points, intensities, question_count)
         answer_sheet.set_data()
+        # print(answer_sheet.question_data)
+        answer_sheet.choose_answers()
+        # print(json.dumps(answer_sheet.question_data))
+        # print(answer_sheet.)
+
+        if args.keysheet:
+            with open(args.keysheet, 'r') as f:
+                key_data: dict[str, str] = json.load(f)
+
+            answer_sheet.set_keydata(key_data)
+            answer_sheet.grade_data()
+            print(json.dumps(answer_sheet.question_data))
+            answer_sheet.mark_choices()
+            cv.imwrite('graded.jpg', answer_sheet.img)
+
+        if args.url:
+            exam = serializers.Exam(1, 1, name="John Doe", control_number="17330462")
+            questions = [serializers.Question(**fields) for fields in answer_sheet.question_data]
+            client = APIClient(args.url)
+            client.post_exam(exam=exam, questions=questions)
+
         # answer_sheet.grade()
         # answer_sheet.mark_grade()
-        cv.imwrite('graded.jpg', answer_sheet.img)
         # AnswerSheet()
 
 if __name__ == '__main__':
